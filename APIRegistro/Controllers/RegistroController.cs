0c55052672d94c1c23566c52effc7ba9124c8f6f
leonelmarinaro <marinaro.leonel@tecnica7.edu.ar>
2023-07-18 10:52:05 -0300

    Agregar archivos de proyecto.

---
+++ APIRegistro/Controllers/RegistroController.cs
@@ -0,0 +1,67 @@
+﻿using APIRegistro.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIRegistro.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RegistroController : ControllerBase
+	{
+		private readonly List<Registro> _registros;
+
+		public RegistroController()
+		{
+			//INICIALIZAMOS LA LISTA DE REGISTROS
+			//ID DNI Nombre Apellido Valido
+			_registros = new List<Registro>
+			{
+				new Registro(){ID=1, DNI="40458641", Nombre="Juan", Apellido="Perez", Valido=true},
+				new Registro(){ID=2, DNI="40458642", Nombre="Juan", Apellido="Perez", Valido=true},
+				new Registro(){ID=3, DNI="40458643", Nombre="Juan", Apellido="Perez", Valido=true},
+				new Registro(){ID=4, DNI="40458644", Nombre="Juan", Apellido="Perez", Valido=true}
+			};
+
+		}
+
+		[HttpGet]
+		public ActionResult<IEnumerable<Registro>> Get()
+		{
+			return _registros;
+		}
+
+		[HttpGet("{id}")]
+		public ActionResult<Registro> Get(int id)
+		{
+			var registro = _registros.FirstOrDefault(r => r.ID == id);
+			if (registro == null)
+			{
+				return NotFound();
+			}
+			return registro;
+		}
+
+		//armar el http post - ponerle un nombre como createRegistro - armar el public actionResult de tipo Post - 
+		//guardar en el registro y luego hacer un save change
+		// ponerlo dentro de un try catch dependiendo si lo pudo hacer o no - entre <> va a devolver un booleano por try catch
+
+		[HttpPost]
+		public ActionResult<bool> CreateRegistro(Registro registro) { 
+			bool estado = false;
+			try
+			{
+				if(registro != null)
+				{
+					_registros.Add(registro);
+					estado = true;
+				}
+			}
+			catch (Exception)
+			{
+				estado = false;
+			}
+			return estado;
+		}
+	}
+}
+//de tarea armar el contexto
---
+++ APIRegistro/Models/Registro.cs
@@ -0,0 +1,13 @@
+﻿namespace APIRegistro.Models
+{
+	public class Registro
+	{
+			public int ID { get; set; }
+			public string DNI { get; set; }
+			public string Nombre { get; set; }
+			public string Apellido { get; set; }
+			public bool Valido { get; set; }
+	}
+}
+
+//ID DNI Nombre Apellido Valido
